5355a66699bfe204cb7c56fea1411955629fd88c
linob <linobotto@gmail.com>
2025-10-28 01:04:09 -0300

    Enhance LeccionDiecisieteScreen by centering content and adding Kodee image

--- app/src/main/java/cl/linobotto/myfirstkotlinapp/view/home/lecciones/leccion_diecisiete/LeccionDiecisieteScreen.kt
+++ app/src/main/java/cl/linobotto/myfirstkotlinapp/view/home/lecciones/leccion_diecisiete/LeccionDiecisieteScreen.kt
@@ -1,6 +1,7 @@
 package cl.linobotto.myfirstkotlinapp.view.home.lecciones.leccion_diecisiete
 
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -22,11 +24,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
+import cl.linobotto.myfirstkotlinapp.R
 import cl.linobotto.myfirstkotlinapp.view.core.navigation.Home
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
@@ -44,11 +48,18 @@ fun LeccionDiecisieteScreen(navController: NavController){
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "Lección 17: YouTubePlayerView en Jetpack Compose",
                 style = MaterialTheme.typography.titleLarge
+            )
+            Image(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp),
+                painter = painterResource(id = R.drawable.kodee_sitting),
+                contentDescription = null
             )
             Spacer(Modifier.height(8.dp))
             SectionBox(title = "¿Qué haremos?") {
